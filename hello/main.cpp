cc1e7faee791827d4f5387411b1c2c0e3dcbb1ab
DaoQing <Dimera@dimera.local>
2016-02-24 11:12:08 +0800

    diyi
    这事第一个

---
+++ hello/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by dimera on 15/12/12.
+//  Copyright (c) 2015年 dimera. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+       cout << "Morning,Hello, World!\n";
+ 
+    return 0;
+}
